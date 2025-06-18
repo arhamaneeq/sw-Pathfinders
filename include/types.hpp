9c1ed11e78fc053eb2b2b5992e5efebca8a62934
Arham Aneeq <arhamaneeq18@gmail.com>
2025-06-18 10:43:12 +0530

    Forward declaration of Coord Struct in types.hpp

--- include/types.hpp
+++ include/types.hpp
@@ -14,66 +14,6 @@
 #include<unordered_map>
 #include<functional>
 
-enum class cellType {
-    Empty,
-    Wall,
-    Start,
-    Goal,
-    Path,
-    Visited
-};
-
-enum class Algorithm {
-    Djikstra,
-    AStar,
-    BFS,
-    DFS
-    // TODO: add support for more algorithms, RRT/RRT*, etc.
-};
-
-enum class State {
-    NO_INIT,
-    INIT,
-    SOLVING,
-    SOLVED
-};
-
-struct Cell {
-    cellType type;
-    bool visited = false;
-    float cost = 1.0f;
-    float heuristic = 0.0f;
-};
-
-namespace Ansi {
-    constexpr const char* Reset = "\033[0m";
-
-    constexpr const char* Bold   = "\033[1m";
-    constexpr const char* Dim    = "\033[2m";
-    constexpr const char* Italic = "\033[3m";
-    constexpr const char* Blink  = "\033[5m"; 
-
-    constexpr const char* Black     = "\033[30m";
-    constexpr const char* Red       = "\033[31m";
-    constexpr const char* Green     = "\033[32m";
-    constexpr const char* Yellow    = "\033[33m";
-    constexpr const char* Blue      = "\033[34m";
-    constexpr const char* Magenta   = "\033[35m";
-    constexpr const char* Cyan      = "\033[36m";
-    constexpr const char* White     = "\033[37m";
-    constexpr const char* DefaultFG = "\033[39m";
-
-    constexpr const char* ClearScreen   = "\033[2J";
-    constexpr const char* ClearLine     = "\033[2K";
-    constexpr const char* CursorHome    = "\033[H";
-    constexpr const char* CursorSave    = "\033[s";
-    constexpr const char* CursorRestore = "\033[u";
-
-    constexpr const char* CursorHide    = "\033[?25l";
-    constexpr const char* CursorShow    = "\033[?25h";
-
-}
-
 struct Coord {
     int x, y;
 
@@ -127,5 +67,68 @@ namespace std {
         }
     };
 };
+
+
+enum class Algorithm {
+    Djikstra,
+    AStar,
+    BFS,
+    DFS
+    // TODO: add support for more algorithms, RRT/RRT*, etc.
+};
+
+enum class State {
+    NO_INIT,
+    INIT,
+    SOLVING,
+    SOLVED
+};
+
+enum class cellType {
+    Empty,
+    Wall,
+    Start,
+    Goal,
+    Path,
+    Visited
+};
+
+struct Cell {
+    cellType type;
+    bool visited = false;
+    float cost = 1.0f;
+    float heuristic = 0.0f;
+    Coord coordinate;
+};
+
+namespace Ansi {
+    constexpr const char* Reset = "\033[0m";
+
+    constexpr const char* Bold   = "\033[1m";
+    constexpr const char* Dim    = "\033[2m";
+    constexpr const char* Italic = "\033[3m";
+    constexpr const char* Blink  = "\033[5m"; 
+
+    constexpr const char* Black     = "\033[30m";
+    constexpr const char* Red       = "\033[31m";
+    constexpr const char* Green     = "\033[32m";
+    constexpr const char* Yellow    = "\033[33m";
+    constexpr const char* Blue      = "\033[34m";
+    constexpr const char* Magenta   = "\033[35m";
+    constexpr const char* Cyan      = "\033[36m";
+    constexpr const char* White     = "\033[37m";
+    constexpr const char* DefaultFG = "\033[39m";
+
+    constexpr const char* ClearScreen   = "\033[2J";
+    constexpr const char* ClearLine     = "\033[2K";
+    constexpr const char* CursorHome    = "\033[H";
+    constexpr const char* CursorSave    = "\033[s";
+    constexpr const char* CursorRestore = "\033[u";
+
+    constexpr const char* CursorHide    = "\033[?25l";
+    constexpr const char* CursorShow    = "\033[?25h";
+
+}
+
 
 #endif
